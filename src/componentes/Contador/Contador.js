29865a61fd337b1dc6c58b0897829f605a27e10a
alete89 <alejandrodini@mural.co>
2020-07-13 21:26:17 -0300

    Contador con componente funcional y hooks

--- src/componentes/Contador/Contador.js
+++ src/componentes/Contador/Contador.js
@@ -1,59 +1,42 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 
-export default class Contador extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            contador: 0
-        }
-    }
+const Contador = () => {
+  const [contador, setContador] = useState(0)
 
-    sumar = () => {
-        this.cambiarContador(this.state.contador + 1)
-    }
+  const sumar = () => {
+    cambiarContador(contador + 1)
+  }
 
-    restar = () => {
-        this.cambiarContador(this.state.contador - 1)
-    }
+  const restar = () => {
+    cambiarContador(contador - 1)
+  }
 
-    cambiarContador = (n) => {
-        this.setState({ contador: n })
-    }
+  const cambiarContador = (n) => {
+    setContador(n)
+  }
 
-    render() {
-        return (
-            <Card>
-                <CardContent>
-                    <Typography gutterBottom variant="h3" component="h2">
-                        Contador
-            </Typography>
-                    <h3 id="contadorValue">
-                        {this.state.contador}
-                    </h3>
-                    <Button
-                        variant="contained"
-                        id="restar"
-                        size="medium"
-                        color="secondary"
-                        onClick={this.restar}>
-                        -
-                    </Button>
-                    &nbsp;
-                    <Button
-                        variant="contained"
-                        id="sumar"
-                        size="medium"
-                        color="primary"
-                        onClick={this.sumar}>
-                        +
-                    </Button>
-                    <br />
-                </CardContent>
-            </Card>
-        )
-    }
+  return (
+    <Card>
+      <CardContent>
+        <Typography gutterBottom variant="h3" component="h2">
+          Contador
+        </Typography>
+        <h3 id="contadorValue">{contador}</h3>
+        <Button variant="contained" id="restar" size="medium" color="secondary" onClick={restar}>
+          -
+        </Button>
+        &nbsp;
+        <Button variant="contained" id="sumar" size="medium" color="primary" onClick={sumar}>
+          +
+        </Button>
+        <br />
+      </CardContent>
+    </Card>
+  )
 }
+
+export default Contador

4bc6986d6feba355a538b678af5f57f98aaf0c94
Fernando Dodino <fernando.dodino@gmail.com>
2022-08-19 23:53:22 -0300

    Fix tests & babel eslint

--- src/componentes/Contador/Contador.test.js
+++ src/componentes/Contador/Contador.test.js
@@ -1,29 +1,30 @@
 import { render } from '@testing-library/react'
 import React from 'react'
+import { act } from 'react-dom/test-utils'
 
 import Contador from './Contador'
 
 describe('Contador', () => {
   describe('cuando se suma', () => {
-    it('el contador incrementa', () => {
+    it('el contador incrementa', async () => {
       const { getByTestId } = render(<Contador />)
       const botonSumar = getByTestId('sumar')
-      botonSumar.click()
-      botonSumar.click()
-      botonSumar.click()
-      const valor = getByTestId('contadorValue')
+      await act(() => { botonSumar.click() })
+      await act(() => { botonSumar.click() })
+      await act(() => { botonSumar.click() })
+      const valor = await getByTestId('contadorValue')
       expect(valor).toHaveTextContent('3')
     })
 
 
   })
   describe('cuando se resta', () => {
-    it('el contador decrementa', () => {
+    it('el contador decrementa', async () => {
       const { getByTestId } = render(<Contador />)
       const botonSumar = getByTestId('restar')
-      botonSumar.click()
-      botonSumar.click()
-      const valor = getByTestId('contadorValue')
+      await act(() => { botonSumar.click() })
+      await act(() => { botonSumar.click() })
+      const valor = await getByTestId('contadorValue')
       expect(valor).toHaveTextContent('-2')
     })
   })

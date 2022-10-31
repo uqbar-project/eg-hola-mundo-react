00812b87b28d366a066a69349bbd73f586b81a1e
Fernando Dodino <fernando.dodino@gmail.com>
2022-10-31 19:15:09 -0300

    CSS for Counter component

--- src/componentes/Contador/Contador.js
+++ src/componentes/Contador/Contador.js
@@ -4,6 +4,7 @@ import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
 import React from 'react'
 import { useState } from 'react'
+import './Contador.css'
 
 export const Contador = (props) => {
   const [contador, setContador] = useState(0)
@@ -19,24 +20,19 @@ export const Contador = (props) => {
   return (
     <Card>
       <CardContent>
-        <Typography gutterBottom variant="h3" component="h2">
+        <Typography variant="h4">
           Contador
           </Typography>
         <h3 data-testid="contadorValue">
           {contador}
         </h3>
-        <Button
-          variant="contained" data-testid="restar" size="medium" color="secondary"
-          onClick={restar}>
-          -
-                  </Button>
-                  &nbsp;
-                  <Button
-          variant="contained" data-testid="sumar" size="medium" color="primary"
-          onClick={sumar}>
-          +
-                  </Button>
-        <br />
+        <div class="botonera">
+          <Button
+            variant="contained" data-testid="restar" size="medium" color="secondary"
+            onClick={restar}>-</Button>
+          <Button variant="contained" data-testid="sumar" size="medium" color="primary"
+          onClick={sumar}>+</Button>
+        </div>
       </CardContent>
     </Card>
   )

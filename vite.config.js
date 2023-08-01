fe5d66a9b3cd46c3ba7a6e32e448d2bcfe248b09
Fernando Dodino <fernando.dodino@gmail.com>
2023-08-01 00:14:29 -0300

    add json summary reporter

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    test: {
-      globals: true,
-      environment: 'jsdom',
-    }
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      reporter: ['text', 'json', 'html', 'json-summary'],
+    },
+  }
 })

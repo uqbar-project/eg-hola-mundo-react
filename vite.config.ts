632b26b8d854f64549aea378fe993ad6cdd0cdec
Fernando Dodino <fernando.dodino@gmail.com>
2024-07-27 09:09:21 -0300

    Using SWC

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
 /// <reference types="vite/client" />
 
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({

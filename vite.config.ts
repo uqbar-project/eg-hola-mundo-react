954fc774f29550b19ccb2a73e18c39a042c8be5e
fdodino <fernando.dodino@gmail.com>
2026-06-09 21:05:32 -0300

    Fix config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
 /// <reference types="vite/client" />
 
 import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
